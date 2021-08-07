113c178cf9ca9839b3cf7744c7a2962eebf3da7c
FudeeSiriwardhana <upekshavirajini07@gmail.com>
2021-08-07 18:03:54 +0530

    initial commit

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
  * this is the first change!
  */
 
+import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
-import App from './App';
+import testprject02 from './src/main';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => testprject02);
